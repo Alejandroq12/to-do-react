952407332850b35ce6c173f2b1202e93be03e7ea
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-12-24 23:51:07 -0600

    Chore: set up project.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edita el archivo <code>src/App.js</code> y guarda para recargar.
+        </p>
+        <a
+          className="App-link"
+          href="https://react.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
