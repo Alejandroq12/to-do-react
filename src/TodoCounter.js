eecbce35a72f25c341b5f7509b9fde6cc70638c7
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-02 13:13:13 -0600

    Feat: style TodoCounter.js

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,6 +1,12 @@
+import './TodoCounter.css';
+
 function TodoCounter({ total, completed }) {
   return (
-    <h1>
+    <h1 style={{
+      fontSize: '24px',
+      textAlign: 'center',
+      margin: 0,
+      padding: '48px' }}>
       You have completed {completed} out of {total} tasks
     </h1>
   );
