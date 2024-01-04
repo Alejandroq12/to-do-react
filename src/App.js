baf501a97d7dd1a7360352881ac50cb4400e5706
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-03 21:05:46 -0600

    Chore: erase unnecessary styles.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import './App.css';
 import React from 'react';
 
 const defaultTodos = [
